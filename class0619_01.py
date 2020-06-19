a9753052d7d358126c3f279c663e461cd3ee75ff
qpwo5470 <37436911+qpwo5470@users.noreply.github.com>
2020-06-19 16:14:06 +0900

    real class 0619

--- class0619_01.py
+++ class0619_01.py
@@ -1,4 +1,15 @@
 list = [1, 3, 5, 7, 9]
+c = list[2:4]
+print(c)
+
+d = list[2:]
+print(d)
+
+e = len(list)
+print(e)
+
+f = list + [11, 13, 15]
+print(f)
+
+list[0] = 1000
 print(list)
-print(list[3])
-print(list[5])
